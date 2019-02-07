8fb0bfe1d84e990e6318028f3b58649e541ed83b
AlanMachadoOSF <alan.machado@osf-commerce.com>
2019-02-07 15:04:45 -0800

    add Quote object and quotes array
    get a random quote and show it

--- js/script.js
+++ js/script.js
@@ -15,9 +15,20 @@ FSJS project 1 - A Random Quote Generator
     - Add at least one `year` and/or `citation` property to at least one 
       quote object.
 ***/
+function Quote(text, author, source, year) {
+  this.text = text;
+  this.author = author;
+  this.source = source;
+  this.year = year;
+}
 
+var q1 = new Quote("Imagination is more important than knowledge. Knowledge is limited. Imagination encircles the world.", "Einstein"),
+    q2 = new Quote("Try not to become a man of success, but rather try to become a man of value.", "Einstein"),
+    q3 = new Quote("Success is not final, failure is not fatal: it is the courage to continue that counts.", "Winston S. Churchill", "Journal", 1944),
+    q4 = new Quote("Bad programmers worry about the code. Good programmers worry about data structures and their relationships.", "Linus Torvalds", "Twitter", 2001),
+    q5 = new Quote("Most good programmers do programming not because they expect to get paid or get adulation by the public, but because it is fun to program.", "Linus Torvalds", "Twitter", 2001);
 
-
+var quotes = new Array(q1, q2, q3, q4, q5);
 
 /***
   Create the `getRandomQuote` function to:
@@ -25,6 +36,10 @@ FSJS project 1 - A Random Quote Generator
    - use the random number to `return` a random quote object from the 
      `quotes` array.
 ***/
+function getRandomQuote() {
+  var index = Math.floor(Math.random() * (quotes.length));
+  return quotes[index];
+}
 
 
 
@@ -38,7 +53,20 @@ FSJS project 1 - A Random Quote Generator
      they are added to the HTML string.
    - set the `innerHTML` of the `quote-box` div to the HTML string. 
 ***/
+function printQuote() {
+  var quote = getRandomQuote(),
+      quoteElement = document.getElementsByClassName('quote')[0],
+      authorElement = document.getElementsByClassName('source')[0];
+    
+  var source = authorElement.childNodes[1];
+  var year = authorElement.childNodes[2];
+    
+  source.innerHTML = quote.source ? quote.source : "Unknown";
+  year.innerHTML = quote.year ? quote.year : "Unknown";
 
+  quoteElement.innerHTML = quote.text;
+  authorElement.innerHTML = quote.author + source.outerHTML + year.outerHTML;
+}
 
 
 
